ec88e3dfed86e62a388493cf99cd121376bef290
cuong <maxwelljenkins2005@gmail.com>
2020-10-03 23:15:17 +0700

    them cai tts.js

---
+++ tts.js
@@ -0,0 +1,16 @@
+let sayHi = function(cb){
+    console.log('run');
+    cb();
+};
+
+let user = {
+    name: 'chinh',
+    cb: function(){
+        console.log('day la ten toi ' + this.name);
+    }
+};
+let user2 = {
+    name: 'cuong'
+};
+
+sayHi(user.cb.bind(user2));
